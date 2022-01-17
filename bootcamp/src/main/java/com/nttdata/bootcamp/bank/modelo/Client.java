564a42e3393dace5b465131a95e16e039c849bf5
Ryoukeshi <andrerl.1990@gmail.com>
2022-01-16 23:17:29 -0500

    AR: 1601 - 2317
    Formato.

---
+++ bootcamp/src/main/java/com/nttdata/bootcamp/bank/modelo/Client.java
@@ -0,0 +1,24 @@
+package com.example.bootcamp.bank.modelo;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "clients")
+public class Client {
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+    private String name;
+    private String client_type;
+    private String status;
+}
